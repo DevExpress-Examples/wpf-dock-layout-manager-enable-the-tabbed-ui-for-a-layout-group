c9c95fd11bde669693a4a9ed894413e648e59545
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Docking_Layout_TabGroups/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Docking_Layout_TabGroups {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to enable the tabbed UI for a layout group
+
+
+<p>This example shows how to create a LayoutGroup, representing its children as tabs. The tabbed interface for the group is enabled via the GroupBorderStyle property.</p>
+
+<br/>
+
+
