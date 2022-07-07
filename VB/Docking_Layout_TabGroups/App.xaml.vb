6aaf1bee71d13a7bfe1b086c649ed666d2432b2b
DevExpressExampleBot <fakeEmail>
2022-07-07 12:50:44 +0400

    Source auto update [skip ci]

---
+++ VB/Docking_Layout_TabGroups/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace Docking_Layout_TabGroups
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
